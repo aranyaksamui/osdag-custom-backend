9a32c8715851bd9946fcf2dc4cee73315b780ed0
Aranyak Samui <aranyaksamuiarya@gmail.com>
2026-08-18 22:45:53 +0530

    Complete with the database models and the session management configurations.

--- server.js
+++ server.js
@@ -1,1 +1,40 @@
-console.log("OSDAG AUTH SYSTEM PROJECT SETUP DONE");
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import session from "express-session";
+import { PrismaSessionStore } from "@quixo3/prisma-session-store";
+import userRoutes from "./routes/user_routes";
+import db from "./database/db";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+// Cors and Session setup
+app.use(cors({ origin: (origin, callback) => callback(null, true), credentials: true }));
+app.use(express.json());
+app.use(session({
+    store: new PrismaSessionStore(db, { 
+        checkPeriod: 2 * 60 * 1000,
+        dbRecordIdIsSessionId: true,
+        dbRecordIdFunction: undefined
+    }),
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 30 * 60 * 1000,
+        httpOnly: true,
+        secure: false,
+        sameSite: "lax"
+    }
+}));
+
+// Routers setup
+app.use("/api/users", userRoutes);
+// app.use("/api/files", fileRoutes);
+
+// Listen on PORT
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`)
+})
